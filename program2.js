9f8989158cf8f3403baf093300ea1c9486fd32a2
R.Pavansai <121311097+R-Pavansai@users.noreply.github.com>
2024-11-11 13:35:51 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,38 @@
 const decodeTheRing = function (s, p) {
+const m = s.length; // Length of the message
+  const n = p.length; // Length of the pattern
 
-    // write your code here
+ 
+  const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(false));
 
+ 
+  dp[0][0] = true;
+
+ 
+  for (let j = 1; j <= n; j++) {
+    if (p[j - 1] === '*') {
+      dp[0][j] = dp[0][j - 1]; // '*' can match zero characters
+    }
+  }
+
+ 
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      const charMessage = s[i - 1];
+      const charPattern = p[j - 1];
+
+      if (charPattern === charMessage || charPattern === '?') {
+       
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (charPattern === '*') {
+      
+        dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+      }
+    }
+  }
+
+ 
+  return dp[m][n];
   };
   
   module.exports = decodeTheRing;
